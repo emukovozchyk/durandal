e08b61287caa608e524736c57233e03e01c90f68
Evgeniy Mukovozchyk <me@stgroupdev.com>
2014-05-07 23:12:18 +0300

    Hello!

---
+++ EmptyDurandal/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace EmptyDurandal.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
---
+++ EmptyDurandal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmptyDurandal.MvcApplication" Language="C#" %>
